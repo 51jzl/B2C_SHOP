dc147608a70b810c5e0c2d1e90fd6fc19c85198c
zhuxianglong <56494565@qq.com>
2016-02-28 18:53:34 +0800

    Revert "1"
    This reverts commit 420a0c01a96a2b9214622a01b27e06c8c6433cb6.

---
+++ shop/Runtime/Cache/Home/ae4339f284e789ed6120d83a49af42eb.php
@@ -0,0 +1,64 @@
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+     <!-- 上述3个meta标签*必须*放在最前面，任何其他内容都*必须*跟随其后！ -->
+    <meta name="description" content="模版">
+    <meta name="author" content="模版">
+	<link rel="icon" href="/shop/Public/images/index.ico">
+    <link rel="stylesheet" href="/shop/Public/css/bootstrap.min.css">
+ 	<!--[if lt IE 9]><script src="/shop/Public/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="/shop/Public/js/ie-emulation-modes-warning.js"></script>
+    <!--[if lt IE 9]>
+      <script src="/shop/Public/js/html5shiv.min.js"></script>
+      <script src="/shop/Public/js/respond.min.js"></script>
+    <![endif]-->
+<title>模版</title>
+</head>
+
+<body>
+	<meta charset="utf-8">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div><!--/.nav-collapse -->
+      </div>
+    </nav>
+	
+	<meta charset="utf-8">
+<style>
+.footer {
+    padding: 40px 0;
+    color: #999;
+    text-align: center;
+    background-color: #f9f9f9;
+    border-top: 1px solid #e5e5e5;
+}</style>
+<footer class="footer">
+      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+    </footer>
+</body>
+<!--加载JS/朱相龙QQ:56494565-->
+	<script src="/shop/Public/js/jquery.min.js"></script>
+	<script src="/shop/Public/js/bootstrap.min.js"></script>
+	<script src="/shop/Public/js/ie10-viewport-bug-workaround.js"></script>
+</html>
