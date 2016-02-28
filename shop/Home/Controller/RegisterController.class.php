420a0c01a96a2b9214622a01b27e06c8c6433cb6
zhuxianglong <56494565@qq.com>
2016-02-28 18:53:22 +0800

    1
    1

--- shop/Home/Controller/RegisterController.class.php
+++
@@ -1,8 +0,0 @@
-<?php
-namespace Home\Controller;
-use Think\Controller;
-class RegisterController extends Controller {
-    public function register(){
-       $this->display('register/register');
-    }
-}
